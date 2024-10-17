5c64e25ee10fe000df96ca2470e471965baca3f2
Mauro0407 <130101420+Mauro0407@users.noreply.github.com>
2024-10-17 09:06:36 -0500

    Add files via upload
    avance

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taller1__Mauricio_Mora.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
